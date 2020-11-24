37d433504ffeb7de33c623d85a414243a50133b4
Vaibhav Saini <sainivaibhav19@gmail.com>
2020-11-24 17:25:14 +0530

    Update server.py

--- python/server.py
+++ python/server.py
@@ -74,7 +74,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/', methods=['POST'])
 @cross_origin()
-def hello():
+def gen_desc():
     data = request.data
     imgdata = base64.b64decode(data[22:])
     image = Image.open(io.BytesIO(imgdata))
